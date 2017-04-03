3b1644f53878ed6a2d175719cdb6849499638669
waynedovey <wdovey@gmail.com>
2017-04-03 22:15:01 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var fs      = require('fs');
 var redis = require('redis-url').connect();
     //redisClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_IP);
     //redisClient = Redis.new(:url => "redis://:6rHRYRaRwUb5X434@redis:6379")
-require("redis-url").parse('redis://:6rHRYRaRwUb5X434@redis:6379')
+require("redis-url").parse('redis://:6rHRYRaRwUb5X434@172.30.116.33:6379')
 
 redis.on("error", function (err) {
   console.error("REDIS Error " + err);
